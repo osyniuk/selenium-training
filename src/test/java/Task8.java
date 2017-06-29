439c6611dd7a7cc41d021e7086b0dc060dc0313e
osyniuk <olena.syniuk@globallogic.com>
2017-06-30 01:00:33 +0300

    Refactored task 8 according to Yaroslav's comments

--- src/test/java/Task8.java
+++ src/test/java/Task8.java
@@ -12,14 +12,18 @@ import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 
 public class Task8 {
     WebDriver driver;
+    WebDriverWait wait;
 
     @Before
     public void start() {
@@ -29,6 +33,7 @@ public class Task8 {
         //driver = new InternetExplorerDriver();
         ChromeDriverManager.getInstance().setup();
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 20);
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
     }
 
@@ -43,24 +48,45 @@ public class Task8 {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.cssSelector(".btn-default")).click();
-        driver.findElement(By.cssSelector(".fa-check-circle"));
+        Assert.assertTrue(driver.findElements(By.cssSelector(".fa-check-circle")).size()>0);
 
         //Open Countries-"Add New Country"
 
         driver.findElement(By.xpath("//span[contains(text(), 'Countries')]")).click();
-        driver.findElement(By.cssSelector("a[href='http://localhost/litecart/admin/?app=countries&doc=edit_country']")).click();
+        driver.findElement(By.xpath("//*[@id='main']/ul/li/a")).click();
 
-        //Open All External Links
+        //Get List of All External Links
 
         List<WebElement> listOfExtLinks = driver.findElements(By.xpath("//label/a/i"));
         for (int i = 0; i < listOfExtLinks.size(); i++) {
+
+            //Get window handles - original and all
+
+            String originalWindowHandle = driver.getWindowHandle();
+            Set<String> listOfWindowHandles = driver.getWindowHandles();
+
+            //click through all links
+
             listOfExtLinks = driver.findElements(By.xpath("//label/a/i"));
             listOfExtLinks.get(i).click();
-            //Assert.assertTrue("Number of browser windows", driver.getWindowHandles().size() == i+2);
+
+            //wait for a new window to open, switch to it, close it
+
+            String newWindow = wait.until(anyWindowOtherThan(listOfWindowHandles));
+            driver.switchTo().window(newWindow);
+            driver.close();
+
+            //switch back to original window
+
+            driver.switchTo().window(originalWindowHandle);
+
+            /*String subWindowHandler = null;
+           Iterator<String> iterator = listOfWindowHandles.iterator();
+            while (iterator.hasNext()) {
+                subWindowHandler = iterator.next();
+                driver.switchTo().window(subWindowHandler);*/
         }
-
-        Assert.assertTrue("Number of browser windows", driver.getWindowHandles().size() == 8);
-
+        // Assert.assertTrue("Number of browser windows", driver.getWindowHandles().size() == 8);
     }
 
     @After
@@ -69,4 +95,14 @@ public class Task8 {
         driver = null;
     }
 
+    public ExpectedCondition<String> anyWindowOtherThan(Set<String> windows) {
+        return new ExpectedCondition<String>() {
+            @Override
+            public String apply(WebDriver input) {
+                Set<String> handles = driver.getWindowHandles();
+                handles.removeAll(windows);
+                return handles.size() > 0 ? handles.iterator().next() : null;
+            }
+        };
+    }
 }
