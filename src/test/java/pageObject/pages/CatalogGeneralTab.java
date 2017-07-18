7fe3dcf714c6a87574b38a659f0136b5ea39229d
osyniuk <olena.syniuk@globallogic.com>
2017-07-18 20:47:12 +0300

    Task 10 = Task 6 via Page Object

--- src/test/java/pageObject/pages/CatalogGeneralTab.java
+++ src/test/java/pageObject/pages/CatalogGeneralTab.java
@@ -10,65 +10,65 @@ import org.openqa.selenium.support.ui.Select;
 import java.io.File;
 
 public class CatalogGeneralTab extends Page {
-    public CatalogGeneralTab(WebDriver driver) {
-        super(driver);
-        PageFactory.initElements(driver, this);
-    }
+  public CatalogGeneralTab(WebDriver driver) {
+    super(driver);
+    PageFactory.initElements(driver, this);
+  }
 
-    public void open(String baseURL) {
-        driver.get(baseURL + "/admin/?category_id=0&app=catalog&doc=edit_product");
-    }
+  public void open(String baseURL) {
+    driver.get(baseURL + "/admin/?category_id=0&app=catalog&doc=edit_product");
+  }
 
-    @FindBy(xpath = "//label[contains(text(), ' Enabled')]")
-    public WebElement enableProduct;
+  @FindBy(xpath = "//label[contains(text(), ' Enabled')]")
+  public WebElement enableProduct;
 
-    @FindBy(xpath = "//input[@name='product_groups[]']")
-    public WebElement productGroupsInput;
+  @FindBy(name = "product_groups[]")
+  public WebElement productGroupsInput;
 
-    @FindBy(xpath = "//input[@name='date_valid_from']")
-    public WebElement dateValidFromInput;
+  @FindBy(name = "date_valid_from")
+  public WebElement dateValidFromInput;
 
-    @FindBy(xpath = "//input[@name='date_valid_to']")
-    public WebElement dateValidToInput;
+  @FindBy(name = "date_valid_to")
+  public WebElement dateValidToInput;
 
-    @FindBy(xpath = "//input[@name='code']")
-    public WebElement codeInput;
+  @FindBy(name = "code")
+  public WebElement codeInput;
 
-    @FindBy(xpath = "//input[@name='name[en]']")
-    public WebElement nameInput;
+  @FindBy(name = "name[en]")
+  public WebElement nameInput;
 
-    @FindBy(xpath = "//input[@name='sku']")
-    public WebElement skuInput;
+  @FindBy(name = "sku")
+  public WebElement skuInput;
 
-    @FindBy(xpath = "//input[@name='gtin']")
-    public WebElement gtinInput;
+  @FindBy(name = "gtin")
+  public WebElement gtinInput;
 
-    @FindBy(xpath = "//input[@name='taric']")
-    public WebElement taricInput;
+  @FindBy(name = "taric")
+  public WebElement taricInput;
 
-    @FindBy(xpath = "//input[@name='quantity']")
-    public WebElement quantityInput;
+  @FindBy(name = "quantity")
+  public WebElement quantityInput;
 
-    @FindBy(xpath = "//input[@name='weight']")
-    public WebElement weightInput;
+  @FindBy(name = "weight")
+  public WebElement weightInput;
 
-    @FindBy(xpath = "//input[@name='dim_x']")
-    public WebElement dimXInput;
+  @FindBy(name = "dim_x")
+  public WebElement dimXInput;
 
-    @FindBy(xpath = "//input[@name='dim_y']")
-    public WebElement dimYInput;
+  @FindBy(name = "dim_y")
+  public WebElement dimYInput;
 
-    @FindBy(xpath = "//input[@name='dim_z']")
-    public WebElement dimZInput;
+  @FindBy(name = "dim_z")
+  public WebElement dimZInput;
 
-    public void selectSoldOutStatus(int statusId) {
-        Select sel = new Select(driver.findElement(By.name("sold_out_status_id")));
-        sel.selectByIndex(statusId);
-    }
+  public void selectSoldOutStatus(int statusId) {
+    Select sel = new Select(driver.findElement(By.name("sold_out_status_id")));
+    sel.selectByIndex(statusId);
+  }
 
-    public void uploadFile(String fileName) {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource(fileName).getFile());
-        driver.findElement(By.xpath("//input[@name='new_images[]']")).sendKeys(file.getAbsolutePath());
-    }
+  public void uploadFile(String fileName) {
+    ClassLoader classLoader = getClass().getClassLoader();
+    File file = new File(classLoader.getResource(fileName).getFile());
+    driver.findElement(By.name("new_images[]")).sendKeys(file.getAbsolutePath());
+  }
 }
