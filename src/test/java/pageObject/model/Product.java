b163af34bd1b60afe9b0cfbb2d0199873d5f4202
osyniuk <olena.syniuk@globallogic.com>
2017-07-19 00:52:03 +0300

    Task 10 = Task 6 via PageObject

--- src/test/java/pageObject/model/Product.java
+++ src/test/java/pageObject/model/Product.java
@@ -2,165 +2,270 @@ package pageObject.model;
 
 public class Product {
 
-  private String dateValidFrom;
-  private String dateValidTo;
-  private String code;
-  private String name;
-  private String sku;
-  private String gtin;
-  private String taric;
-  private String quantity;
-  private String weight;
-  private String dimX;
-  private String dimY;
-  private String dimZ;
-  private int statusId;
-  private String fileName;
-  private int manId;
-  private String keywords;
-  private String shortDescription;
-  private String description;
-  private String attributes;
-  private String headTitle;
-  private String metaDescription;
-  private String purchasePrice;
-  private String currencyCode;
-  private String USDPrice;
-  private String EURPrice;
+    private String dateValidFrom;
+    private String dateValidTo;
+    private String code;
+    private String name;
+    private String sku;
+    private String gtin;
+    private String taric;
+    private String quantity;
+    private String weight;
+    private String dimX;
+    private String dimY;
+    private String dimZ;
+    private int statusId;
+    private String fileName;
+    private int manId;
+    private String keywords;
+    private String shortDescription;
+    private String description;
+    private String attributes;
+    private String headTitle;
+    private String metaDescription;
+    private String purchasePrice;
+    private String currencyCode;
+    private String USDPrice;
+    private String EURPrice;
 
-  public static Builder newEntity() {
-    return new Product().new Builder();
-  }
-  public String getDateValidFrom() {
-    return dateValidFrom;
-  }
-
-  public String getDateValidTo() {
-    return dateValidTo;
-  }
-
-  public String getCode() {
-    return code;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getSku() {
-    return sku;
-  }
-
-  public String getGtin() {
-    return gtin;
-  }
-
-  public String getTaric() {
-    return taric;
-  }
-
-  public String getQuantity() {
-    return quantity;
-  }
-
-  public String getWeight() {
-    return weight;
-  }
-
-  public String getDimX() {
-    return dimX;
-  }
-
-  public String getDimY() {
-    return dimY;
-  }
-
-  public String getDimZ() {
-    return dimZ;
-  }
-
-  public int getStatusId() {
-    return statusId;
-  }
-
-  public String getFileName() {
-    return fileName;
-  }
-
-  public int getManId() {
-    return manId;
-  }
-
-  public String getKeywords() {
-    return keywords;
-  }
-
-  public String getShortDescription() {
-    return shortDescription;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public String getAttributes() {
-    return attributes;
-  }
-
-  public String getHeadTitle() {
-    return headTitle;
-  }
-
-  public String getMetaDescription() {
-    return metaDescription;
-  }
-
-  public String getPurchasePrice() {
-    return purchasePrice;
-  }
-
-  public String getCurrencyCode() {
-    return currencyCode;
-  }
-
-  public String getUSDPrice() {
-    return USDPrice;
-  }
-
-  public String getEURPrice() {
-    return EURPrice;
-  }
-
-  public class Builder {
-    private Builder() {}
-    public Builder withDateValidFrom (String dateValidFrom) {Product.this.dateValidFrom=dateValidFrom; return this;}
-    public Builder withDateValidTo (String dateValidTo) {Product.this.dateValidTo=dateValidTo; return this;}
-    public Builder withCode (String code) {Product.this.code=code; return this;}
-    public Builder withName (String name) {Product.this.name=name; return this;}
-    public Builder withSku (String sku) {Product.this.sku=sku; return this;}
-    public Builder withGtin (String gtin) {Product.this.gtin=gtin; return this;}
-    public Builder withTaric (String taric) {Product.this.taric=taric; return this;}
-    public Builder withQuantity (String quantity) {Product.this.quantity=quantity; return this;}
-    public Builder withWeight (String weight) {Product.this.weight=weight; return this;}
-    public Builder withDimX (String dimX) {Product.this.dimX=dimX; return this;}
-    public Builder withDimY (String dimY) {Product.this.dimY=dimY; return this;}
-    public Builder withDimZ (String dimZ) {Product.this.dimZ=dimZ; return this;}
-    public Builder withStatusId (int statusId) {Product.this.statusId=statusId; return this;}
-    public Builder withFileName (String fileName) {Product.this.fileName=fileName; return this;}
-    public Builder withManId (int manId) {Product.this.manId=manId; return this;}
-    public Builder withKeywords (String keywords) {Product.this.keywords=keywords; return this;}
-    public Builder withShortDescription (String shortDescription) {Product.this.shortDescription=shortDescription; return this;}
-    public Builder withDescription (String description) {Product.this.description=description; return this;}
-    public Builder withAttributes (String attributes) {Product.this.attributes=attributes; return this;}
-    public Builder withHeadTitle (String headTitle) {Product.this.headTitle=headTitle; return this;}
-    public Builder withMetaDescription (String metaDescription) {Product.this.metaDescription=metaDescription; return this;}
-    public Builder withPurchasePrice (String purchasePrice) {Product.this.purchasePrice=purchasePrice; return this;}
-    public Builder withCurrencyCode (String currencyCode) {Product.this.currencyCode=currencyCode; return this;}
-    public Builder withUSDPrice (String USDPrice) {Product.this.USDPrice=USDPrice; return this;}
-    public Builder withEURPrice (String EURPrice) {Product.this.EURPrice=EURPrice; return this;}
+    public static Builder newEntity() {
+        return new Product().new Builder();
+    }
 
 
+    public String getDateValidFrom() {
+        return dateValidFrom;
+    }
 
-    public Product build() {return Product.this;}
-  }
+    public String getDateValidTo() {
+        return dateValidTo;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getGtin() {
+        return gtin;
+    }
+
+    public String getTaric() {
+        return taric;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public String getDimX() {
+        return dimX;
+    }
+
+    public String getDimY() {
+        return dimY;
+    }
+
+    public String getDimZ() {
+        return dimZ;
+    }
+
+    public int getStatusId() {
+        return statusId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getManId() {
+        return manId;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAttributes() {
+        return attributes;
+    }
+
+    public String getHeadTitle() {
+        return headTitle;
+    }
+
+    public String getMetaDescription() {
+        return metaDescription;
+    }
+
+    public String getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public String getUSDPrice() {
+        return USDPrice;
+    }
+
+    public String getEURPrice() {
+        return EURPrice;
+    }
+
+    public class Builder {
+        private Builder() {
+        }
+
+
+        public Builder withDateValidFrom(String dateValidFrom) {
+            Product.this.dateValidFrom = dateValidFrom;
+            return this;
+        }
+
+        public Builder withDateValidTo(String dateValidTo) {
+            Product.this.dateValidTo = dateValidTo;
+            return this;
+        }
+
+        public Builder withCode(String code) {
+            Product.this.code = code;
+            return this;
+        }
+
+        public Builder withName(String name) {
+            Product.this.name = name;
+            return this;
+        }
+
+        public Builder withSku(String sku) {
+            Product.this.sku = sku;
+            return this;
+        }
+
+        public Builder withGtin(String gtin) {
+            Product.this.gtin = gtin;
+            return this;
+        }
+
+        public Builder withTaric(String taric) {
+            Product.this.taric = taric;
+            return this;
+        }
+
+        public Builder withQuantity(String quantity) {
+            Product.this.quantity = quantity;
+            return this;
+        }
+
+        public Builder withWeight(String weight) {
+            Product.this.weight = weight;
+            return this;
+        }
+
+        public Builder withDimX(String dimX) {
+            Product.this.dimX = dimX;
+            return this;
+        }
+
+        public Builder withDimY(String dimY) {
+            Product.this.dimY = dimY;
+            return this;
+        }
+
+        public Builder withDimZ(String dimZ) {
+            Product.this.dimZ = dimZ;
+            return this;
+        }
+
+        public Builder withStatusId(int statusId) {
+            Product.this.statusId = statusId;
+            return this;
+        }
+
+        public Builder withFileName(String fileName) {
+            Product.this.fileName = fileName;
+            return this;
+        }
+
+        public Builder withManId(int manId) {
+            Product.this.manId = manId;
+            return this;
+        }
+
+        public Builder withKeywords(String keywords) {
+            Product.this.keywords = keywords;
+            return this;
+        }
+
+        public Builder withShortDescription(String shortDescription) {
+            Product.this.shortDescription = shortDescription;
+            return this;
+        }
+
+        public Builder withDescription(String description) {
+            Product.this.description = description;
+            return this;
+        }
+
+        public Builder withAttributes(String attributes) {
+            Product.this.attributes = attributes;
+            return this;
+        }
+
+        public Builder withHeadTitle(String headTitle) {
+            Product.this.headTitle = headTitle;
+            return this;
+        }
+
+        public Builder withMetaDescription(String metaDescription) {
+            Product.this.metaDescription = metaDescription;
+            return this;
+        }
+
+        public Builder withPurchasePrice(String purchasePrice) {
+            Product.this.purchasePrice = purchasePrice;
+            return this;
+        }
+
+        public Builder withCurrencyCode(String currencyCode) {
+            Product.this.currencyCode = currencyCode;
+            return this;
+        }
+
+        public Builder withUSDPrice(String USDPrice) {
+            Product.this.USDPrice = USDPrice;
+            return this;
+        }
+
+        public Builder withEURPrice(String EURPrice) {
+            Product.this.EURPrice = EURPrice;
+            return this;
+        }
+
+
+        public Product build() {
+            return Product.this;
+        }
+    }
 }
--- src/test/java/pageObject/pages/CatalogGeneralTab.java
+++ src/test/java/pageObject/pages/CatalogGeneralTab.java
@@ -10,65 +10,65 @@ import org.openqa.selenium.support.ui.Select;
 import java.io.File;
 
 public class CatalogGeneralTab extends Page {
-  public CatalogGeneralTab(WebDriver driver) {
-    super(driver);
-    PageFactory.initElements(driver, this);
-  }
+    public CatalogGeneralTab(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
 
-  public void open(String baseURL) {
-    driver.get(baseURL + "/admin/?category_id=0&app=catalog&doc=edit_product");
-  }
+    public void open(String baseURL) {
+        driver.get(baseURL + "/admin/?category_id=0&app=catalog&doc=edit_product");
+    }
 
-  @FindBy(xpath = "//label[contains(text(), ' Enabled')]")
-  public WebElement enableProduct;
+    @FindBy(xpath = "//label[contains(text(), ' Enabled')]")
+    public WebElement enableProduct;
 
-  @FindBy(name = "product_groups[]")
-  public WebElement productGroupsInput;
+    @FindBy(xpath = "//input[@value='1-3']")
+    public WebElement productGroupsInput;
 
-  @FindBy(name = "date_valid_from")
-  public WebElement dateValidFromInput;
+    @FindBy(name = "date_valid_from")
+    public WebElement dateValidFromInput;
 
-  @FindBy(name = "date_valid_to")
-  public WebElement dateValidToInput;
+    @FindBy(name = "date_valid_to")
+    public WebElement dateValidToInput;
 
-  @FindBy(name = "code")
-  public WebElement codeInput;
+    @FindBy(name = "code")
+    public WebElement codeInput;
 
-  @FindBy(name = "name[en]")
-  public WebElement nameInput;
+    @FindBy(name = "name[en]")
+    public WebElement nameInput;
 
-  @FindBy(name = "sku")
-  public WebElement skuInput;
+    @FindBy(name = "sku")
+    public WebElement skuInput;
 
-  @FindBy(name = "gtin")
-  public WebElement gtinInput;
+    @FindBy(name = "gtin")
+    public WebElement gtinInput;
 
-  @FindBy(name = "taric")
-  public WebElement taricInput;
+    @FindBy(name = "taric")
+    public WebElement taricInput;
 
-  @FindBy(name = "quantity")
-  public WebElement quantityInput;
+    @FindBy(name = "quantity")
+    public WebElement quantityInput;
 
-  @FindBy(name = "weight")
-  public WebElement weightInput;
+    @FindBy(name = "weight")
+    public WebElement weightInput;
 
-  @FindBy(name = "dim_x")
-  public WebElement dimXInput;
+    @FindBy(name = "dim_x")
+    public WebElement dimXInput;
 
-  @FindBy(name = "dim_y")
-  public WebElement dimYInput;
+    @FindBy(name = "dim_y")
+    public WebElement dimYInput;
 
-  @FindBy(name = "dim_z")
-  public WebElement dimZInput;
+    @FindBy(name = "dim_z")
+    public WebElement dimZInput;
 
-  public void selectSoldOutStatus(int statusId) {
-    Select sel = new Select(driver.findElement(By.name("sold_out_status_id")));
-    sel.selectByIndex(statusId);
-  }
+    public void selectSoldOutStatus(int statusId) {
+        Select sel = new Select(driver.findElement(By.name("sold_out_status_id")));
+        sel.selectByIndex(statusId);
+    }
 
-  public void uploadFile(String fileName) {
-    ClassLoader classLoader = getClass().getClassLoader();
-    File file = new File(classLoader.getResource(fileName).getFile());
-    driver.findElement(By.name("new_images[]")).sendKeys(file.getAbsolutePath());
-  }
+    public void uploadFile(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+        driver.findElement(By.name("new_images[]")).sendKeys(file.getAbsolutePath());
+    }
 }
