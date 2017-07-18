3ff5006551c21a630cefd35f4cfaa7b2a2082bfc
osyniuk <olena.syniuk@globallogic.com>
2017-07-18 20:52:57 +0300

    Task 10 = Task 6 via Page Object

--- src/test/java/pageObject/tests/TestBase.java
+++ src/test/java/pageObject/tests/TestBase.java
@@ -5,21 +5,21 @@ import pageObject.app.Application;
 
 public class TestBase {
 
-    public static ThreadLocal<Application>tlApp = new ThreadLocal<>();
-    public Application app;
+  public static ThreadLocal<Application>tlApp = new ThreadLocal<>();
+  public Application app;
 
-    @Before
-    public void start() {
-        if (tlApp.get() !=null) {
-            app = tlApp.get();
-            return;
-        }
-
-        app = new Application();
-        tlApp.set(app);
-
-        Runtime.getRuntime().addShutdownHook(
-                new Thread(() -> {app.stop(); app=null;}
-        ));
+  @Before
+  public void start() {
+    if (tlApp.get() !=null) {
+      app = tlApp.get();
+      return;
     }
+
+    app = new Application();
+    tlApp.set(app);
+
+    Runtime.getRuntime().addShutdownHook(
+      new Thread(() -> {app.stop(); app=null;}
+      ));
+  }
 }
