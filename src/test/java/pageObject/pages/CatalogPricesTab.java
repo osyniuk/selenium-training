9a677776902451a5db235b361b2e3f0e11f4f442
osyniuk <olena.syniuk@globallogic.com>
2017-07-18 20:49:37 +0300

    Task 10 = Task 6 via Page Object

--- src/test/java/pageObject/pages/CatalogPricesTab.java
+++ src/test/java/pageObject/pages/CatalogPricesTab.java
@@ -9,29 +9,29 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 public class CatalogPricesTab extends Page{
-    public CatalogPricesTab(WebDriver driver) {
-        super(driver);
-        PageFactory.initElements(driver, this);
-    }
-    public void open(String baseURL) {
-        driver.findElement(By.xpath("//a[contains(text(), 'Prices')]")).click();//!!!
-    }
+  public CatalogPricesTab(WebDriver driver) {
+    super(driver);
+    PageFactory.initElements(driver, this);
+  }
+  public void open() {
+    driver.findElement(By.xpath("//a[contains(text(), 'Prices')]")).click();
+  }
 
-    @FindBy(xpath = "//input[@name='purchase_price']")
-    public WebElement purchasePriceInput;
+  @FindBy(name = "purchase_price")
+  public WebElement purchasePriceInput;
 
-    public void selectCurrencyCode (String value) {
-        Select sel1 = new Select(driver.findElement(By.name("purchase_price_currency_code")));
-        sel1.selectByValue(value);
-    }
-    @FindBy(xpath = "//input[@name='prices[USD]']")
-    public WebElement USDPriceInput;
+  public void selectCurrencyCode (String value) {
+    Select sel1 = new Select(driver.findElement(By.name("purchase_price_currency_code")));
+    sel1.selectByValue(value);
+  }
+  @FindBy(name = "prices[USD]")
+  public WebElement USDPriceInput;
 
-    @FindBy(xpath = "//input[@name='prices[EUR]']")
-    public WebElement EURPriceInput;
+  @FindBy(name = "prices[EUR]")
+  public WebElement EURPriceInput;
 
-   public void save () {
-       driver.findElement(By.xpath("//button[@name='save']")).click();
-   }
+  public void save () {
+    driver.findElement(By.name("save")).click();
+  }
 
 }
