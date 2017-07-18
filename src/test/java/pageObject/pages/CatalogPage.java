9a6f78ae1c1d60512490dadf5edd3e1ec1fa51b4
osyniuk <olena.syniuk@globallogic.com>
2017-07-18 20:48:56 +0300

    Task 10 = Task 6 via Page Object

--- src/test/java/pageObject/pages/CatalogPage.java
+++ src/test/java/pageObject/pages/CatalogPage.java
@@ -2,23 +2,22 @@ package pageObject.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CatalogPage extends Page {
 
-    public CatalogPage(WebDriver driver) {
-        super(driver);
-        PageFactory.initElements(driver, this);
-    }
+  public CatalogPage(WebDriver driver) {
+    super(driver);
+    PageFactory.initElements(driver, this);
+  }
 
-    public CatalogPage open(String baseURL) {
-        driver.get(baseURL + "/admin/?app=catalog&doc=catalog");
-        return this;
-    }
+  public CatalogPage open(String baseURL) {
+    driver.get(baseURL + "/admin/?app=catalog&doc=catalog");
+    return this;
+  }
 
-    public Integer getNumberOfProducts() {
-        return driver.findElements(By.xpath("//*[@id='main']/form/table/tbody/tr")).size();
-    }
+  public Integer getNumberOfProducts() {
+    return driver.findElements(By.xpath("//*[@id='main']/form/table/tbody/tr")).size();
+  }
 
 }
