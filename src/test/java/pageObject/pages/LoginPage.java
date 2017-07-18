b48ee34ffa0c0f7ad07d00d2f5f6619890551cc9
osyniuk <olena.syniuk@globallogic.com>
2017-07-18 20:50:12 +0300

    Task 10 = Task 6 via Page Object

--- src/test/java/pageObject/pages/LoginPage.java
+++ src/test/java/pageObject/pages/LoginPage.java
@@ -4,32 +4,32 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage extends Page {
-    public LoginPage(WebDriver driver) {
-        super(driver);
-    }
-    public LoginPage open (String baseURL) {
-        driver.get(baseURL + "/admin");
-        return this;
-    }
+  public LoginPage(WebDriver driver) {
+    super(driver);
+  }
+  public LoginPage open (String baseURL) {
+    driver.get(baseURL + "/admin");
+    return this;
+  }
 
-    public boolean isOnLoginPage () {
-        return driver.findElements(By.id("box-login")).size()>0;
-    }
+  public boolean isOnLoginPage () {
+    return driver.findElements(By.id("box-login")).size()>0;
+  }
 
-    public LoginPage enterUsername (String username) {
-        driver.findElement(By.name("username")).sendKeys("admin");
-        return this;
-    }
+  public LoginPage enterUsername (String username) {
+    driver.findElement(By.name("username")).sendKeys("admin");
+    return this;
+  }
 
-    public LoginPage enterPassword (String password) {
-        driver.findElement(By.name("password")).sendKeys("admin");
-        return this;
-    }
+  public LoginPage enterPassword (String password) {
+    driver.findElement(By.name("password")).sendKeys("admin");
+    return this;
+  }
 
-    public void submitLogin () {
-        driver.findElement(By.cssSelector(".btn-default")).click();
-        wait.until((WebDriver d) -> d.findElement(By.cssSelector(".fa-check-circle")));
-    }
+  public void submitLogin () {
+    driver.findElement(By.cssSelector(".btn-default")).click();
+    wait.until((WebDriver d) -> d.findElement(By.cssSelector(".fa-check-circle")));
+  }
 
 
 
